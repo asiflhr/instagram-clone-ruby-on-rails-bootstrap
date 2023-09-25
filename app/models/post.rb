c248f1b9523b75390582ce58921c984b1327754a
Muhammad Asif <asif88383@gmail.com>
2023-09-26 01:35:49 +0500

    added image storage functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
     validates :keywords, presence: true, length: {minimum: 5, maximum: 300}
+
+    has_many_attached :images
 end
